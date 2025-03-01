1aa8970a01a1c855258bea1fe2180c9fdc46cb54
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-03-01 15:59:11 +0600

    improved time function and fixing from assignment video

--- script.js
+++ script.js
@@ -21,13 +21,18 @@ function initDate() {
     const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
     var dateObj = new Date();
+    const options = {hour12: true};
+    const date12hr = dateObj.toLocaleString('en-US', options).split(",");
+    console.log(date12hr);
 
     dateInfo = {
         "month": months[dateObj.getMonth()],
         "day": days[dateObj.getDay()],
         "date": dateObj.getDate(),
+        "date12hr": date12hr[0],
         "year": dateObj.getFullYear(),
-        "time": `${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`
+        "time": `${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`,
+        "time12hr": date12hr[1]
     }
 
     var dateElem = document.querySelector("#date");
@@ -45,9 +50,10 @@ function completeTask(cardBody) {
     const div = document.createElement("div");
     div.classList.add("text-sm", "bg-[#F4F7FF]", "ml-4", "mr-4", "mt-4", "p-3", "rounded-md")
     initDate();
-    const node = document.createTextNode(`You have Completed The Task ${title} at ${dateInfo.time}`);
+    const node = document.createTextNode(`You have Completed The Task ${title} at ${dateInfo.time12hr}`);
 
     div.appendChild(node);
+    alert("Board Updated Successfully");
     document.querySelector("#completed-tasks-container").appendChild(div);
 
     cardBody.querySelector(".complete-btn").classList.add("btn-disabled")
